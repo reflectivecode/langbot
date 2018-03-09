56a9c935f9fdcacea6325c9e86028012fafd3fdb
Greg <reflectivecode@users.noreply.github.com>
2018-03-08 23:09:46 -0600

    make command text optional

--- app/web/Slack/CommandRequest.cs
+++ app/web/Slack/CommandRequest.cs
@@ -7,7 +7,7 @@ namespace LangBot.Web.Slack
     public class CommandRequest : SlackRequest
     {
         [FromForm(Name = "command"), Required] public string Command { get; set; }
-        [FromForm(Name = "text"), Required] public string Text { get; set; }
+        [FromForm(Name = "text")] public string Text { get; set; } = "";
         [FromForm(Name = "user_id"), Required] public string UserId { get; set; }
     }
 }
