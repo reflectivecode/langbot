7e49e50116d63d7d23affd44b0104d8badc178ba
Greg <reflectivecode@users.noreply.github.com>
2018-03-22 00:26:18 -0500

    don't allow text to be null

--- app/web/Slack/Models/SlackCommandRequest.cs
+++ app/web/Slack/Models/SlackCommandRequest.cs
@@ -5,8 +5,10 @@ namespace LangBot.Web.Slack
 {
     public class SlackCommandRequest : ISlackRequest
     {
+        private string _text;
+
         [FromForm(Name = "command"), Required] public string Command { get; set; }
-        [FromForm(Name = "text")] public string Text { get; set; } = "";
+        [FromForm(Name = "text")] public string Text { get => _text ?? ""; set => _text = value; }
         [FromForm(Name = "token"), Required] public string Token { get; set; }
         [FromForm(Name = "team_id"), Required] public string TeamId { get; set; }
         [FromForm(Name = "team_domain"), Required] public string TeamDomain { get; set; }
